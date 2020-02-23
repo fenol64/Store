6feb901863bc4baa6a3d6150125c0877b4a48988
NOX-0202 <fernandinho.nco@gmail.com>
2020-02-22 23:12:34 -0300

    final database

--- store.sql
+++ store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 26-Jan-2020 às 22:56
+-- Tempo de geração: 23-Fev-2020 às 03:07
 -- Versão do servidor: 10.4.11-MariaDB
 -- versão do PHP: 7.4.1
 
@@ -35,6 +35,13 @@ CREATE TABLE `isopened` (
   `status` varchar(7) DEFAULT 'aberto'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `isopened`
+--
+
+INSERT INTO `isopened` (`id_isopened`, `time_inicial`, `time_final`, `status`) VALUES
+(14, '2020-02-23 01:37:05', '1970-01-01 03:00:01', 'aberto');
+
 -- --------------------------------------------------------
 
 --
@@ -46,7 +53,8 @@ CREATE TABLE `orderbody` (
   `id_order` varchar(6) NOT NULL,
   `name_product` varchar(40) NOT NULL DEFAULT 'none',
   `value_product` float NOT NULL DEFAULT 0,
-  `status_order` varchar(10) NOT NULL DEFAULT 'pendente'
+  `status_order` varchar(10) NOT NULL DEFAULT 'pendente',
+  `created_At` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -61,8 +69,15 @@ CREATE TABLE `orders` (
   `total` float NOT NULL DEFAULT 0,
   `day_inserted` varchar(11) NOT NULL DEFAULT '',
   `status` varchar(10) NOT NULL DEFAULT 'pendente',
-  `created_At` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_At` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `orders`
+--
+
+INSERT INTO `orders` (`id_order`, `id`, `total`, `day_inserted`, `status`, `created_At`) VALUES
+('68866', 152, 0, '14', 'pendente', '2020-02-22');
 
 -- --------------------------------------------------------
 
@@ -74,7 +89,9 @@ CREATE TABLE `payment` (
   `id` int(11) NOT NULL,
   `id_order` varchar(6) NOT NULL,
   `method` varchar(15) NOT NULL DEFAULT 'dinheiro',
-  `value_paid` float NOT NULL DEFAULT 0
+  `value_paid` float NOT NULL DEFAULT 0,
+  `created_At` date NOT NULL DEFAULT current_timestamp(),
+  `status_order` varchar(12) NOT NULL DEFAULT 'pendente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -88,6 +105,27 @@ CREATE TABLE `products` (
   `name_product` varchar(40) NOT NULL DEFAULT 'none',
   `value_product` float NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `products`
+--
+
+INSERT INTO `products` (`id_product`, `name_product`, `value_product`) VALUES
+(1, 'Batata comum P.P', 5),
+(2, 'Batata comum Pequena', 10),
+(3, 'Batata comum Média', 15),
+(4, 'Batata comum Grande', 20),
+(5, 'Batata comum Gigante', 25),
+(6, 'Batata Americana Pequena', 20),
+(7, 'Batata Americana Média', 25),
+(8, 'Batata Americana Grande', 30),
+(9, 'Batata Americana Gigante', 40),
+(10, ' 1 - Latão', 5),
+(11, '3 - Latões', 14),
+(12, 'Refrigerante', 5),
+(13, 'Guaracamp', 2),
+(14, 'Água c/ gás', 3),
+(15, 'Água s/ gás', 2);
 
 -- --------------------------------------------------------
 
@@ -158,38 +196,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de tabela `isopened`
 --
 ALTER TABLE `isopened`
-  MODIFY `id_isopened` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id_isopened` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `orderbody`
 --
 ALTER TABLE `orderbody`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=152;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `products`
 --
 ALTER TABLE `products`
-  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_product` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
